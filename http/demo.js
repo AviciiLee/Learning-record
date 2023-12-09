f1ca9057b6a90f40ff5a902137196970ac7b3e19
AviciiLee <lx_ashin@aliyun.com>
2023-12-10 00:54:34 +0800

    feat: 图解http

---
+++ http/demo.js
@@ -0,0 +1,19 @@
+const net = require('net')
+
+const client = net.createConnection(
+  {
+    host: 'www.baidu.com',
+    port: 80
+  },
+  () => {
+    client.write(`GET / HTTP/1.1\r\n
+    Host: www.baidu.com\r\n
+    Cookie: BAIDUID=818763236A41AB723EE45626F70C2555:FG=1;`)
+    console.log('connected to server!')
+  }
+)
+
+client.on('data', (chunk) => {
+  console.log(chunk.toString())
+  client.end()
+})
